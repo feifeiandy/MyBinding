8592b54d8648e65f413a391ae6ec9a19194d1d0f
feifeiandy <feifeiandy@outlook.com>
2016-07-24 18:30:04 +0800

    x

---
+++ WpfApplication2/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModel vvm;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            vvm = new ViewModel();
+            this.ls.ItemsSource = vvm.stus;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.vvm.Add(Convert.ToInt32(this.txtID.Text), this.txtName.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.vvm.dele(Convert.ToInt32(this.txtID.Text));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.vvm.update(Convert.ToInt32(this.txtID.Text), this.txtName.Text);
+        }
+    }
+}
---
+++ WpfApplication2/WpfApplication2/ViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication2
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Student> stus;
+        public ViewModel()
+        {
+            this.stus = new ObservableCollection<Student>() { 
+            new Student(){ ID=1, Name="张三"},
+            new Student(){ ID=2, Name="李四"}
+            
+            };
+        }
+
+        public void Add(int id,string name)
+        {
+            Student stu = new Student() {ID=id,Name=name };
+            this.stus.Add(stu);
+        }
+
+        public void dele(int id)
+        {
+            this.stus.RemoveAt(id-1);
+        }
+
+        public void update(int id,string name)
+        {
+            this.stus.First(x => x.ID == id).Name = name;
+        }
+       
+    }
+
+    public class Student:INotifyPropertyChanged
+    {
+        int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value;
+            this.setv("ID");
+            }
+        }
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value;
+            this.setv("Name");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void setv(string name)
+        {
+            if (this.PropertyChanged!=null)
+            {
+                this.PropertyChanged.Invoke(this,new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
